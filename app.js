b469d2dd494bafc9330993cfa528a5ebceab230a
phil <dev@chutter.co>
2015-08-20 10:55:26 -0500

    fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/workers');
+var workers = require('./routes/workers');
 
 var app = express();
 
