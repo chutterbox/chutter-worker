eca49b4b33b8d4b369ee24a6e799549ae23acfa6
phil <dev@chutter.co>
2015-08-20 12:12:29 -0500

    should probably respond to the request

--- routes/workers.js
+++ routes/workers.js
@@ -5,6 +5,7 @@ var AWS = require("aws-sdk");
 /* GET users listing. */
 router.post('/screencap', function(req, res, next) {
   console.log("here here");
+  res.send(200);
   // aws_akid = process.env.S3_AWS_ACCESS_KEY_ID
   // aws_sk = process.env.S3_AWS_SECRET_ACCESS_KEY
   // aws_region = process.env.S3_AWS_REGION
