db55ee8e01743302ad66ee648dcf730fc3126c0b
phil <dev@chutter.co>
2015-08-20 17:01:40 -0500

    boom

--- routes/workers.js
+++ routes/workers.js
@@ -4,30 +4,39 @@ var webshot = require("webshot");
 var AWS = require("aws-sdk");
 /* GET users listing. */
 router.post('/screencap', function(req, res, next) {
-  console.log(req);
-  console.log(JSON.stringify(req.body));
-  // image_stream = webshot url, profiles[profile]
+  var params = JSON.parse(req.body);
+  console.log(params.screencap_url);
+  var screencap_url = params.screencap_url;
 
-  // image_buffers = []
-  // image_stream.on "data", (buffer) ->
-  //   image_buffers.push buffer
-  
-  // image_stream.on "error", (err) ->
-  //   res.reply "Webshot error: " + err
-  AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    region: 'us-west-1'
-  });
-  sqs = new AWS.SQS();
-  res.json(200);
-  // aws_akid = process.env.S3_AWS_ACCESS_KEY_ID
-  // aws_sk = process.env.S3_AWS_SECRET_ACCESS_KEY
-  // aws_region = process.env.S3_AWS_REGION
-  // aws_s3_bucket = process.env.WEBSHOT_S3_BUCKET
-  // AWS.config.update({accessKeyId: aws_akid, secretAccessKey: aws_sk, region: aws_region});
-  // s3bucket = new AWS.S3({params: {Bucket: aws_s3_bucket}})
-  // res.send('respond with a resource');
+  if(!!screencap_url){
+    var image_stream = webshot('https://www.facebook.com/', {quality: 100});
+    var image_buffers = [];
+    image_stream.on("data", function(buffer){
+        image_buffers.push(buffer);
+      }
+    )
+    image_stream.on("error", function(err){
+      res.reply("Webshot error: " + err);
+    })
+  }
+  aws_akid = process.env.S3_AWS_ACCESS_KEY_ID
+  aws_sk = process.env.S3_AWS_SECRET_ACCESS_KEY
+  aws_region = process.env.S3_AWS_REGION
+  aws_s3_bucket = process.env.WEBSHOT_S3_BUCKET
+  AWS.config.update({accessKeyId: aws_akid, secretAccessKey: aws_sk, region: aws_region});
+  s3bucket = new AWS.S3({params: {Bucket: "chutter-screencaps"}})
+  image_stream.on("end", function(){
+    image_data = Buffer.concat(image_buffers);
+    params = {Key: (Math.random(1000)+".jpg"), Body: image_data, ContentType: "image/jpg"}
+    s3bucket.upload(params, function(err, data){
+      if(err){
+        res.send("S3 Upload error: " + err);
+      }
+      else{
+        res.send(data["Location"]);
+      }
+    })
+  })
 });
 
 module.exports = router;
