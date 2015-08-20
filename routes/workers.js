71a0d1bc0b02ba85f5a8bc2f8755e9e2d88d2705
phil <dev@chutter.co>
2015-08-20 17:32:37 -0500

    eh

--- routes/workers.js
+++ routes/workers.js
@@ -18,25 +18,26 @@ router.post('/screencap', function(req, res, next) {
       image_stream.on("error", function(err){
         res.reply("Webshot error: " + err);
       })
-    }
-    aws_akid = process.env.S3_AWS_ACCESS_KEY_ID
-    aws_sk = process.env.S3_AWS_SECRET_ACCESS_KEY
-    aws_region = process.env.S3_AWS_REGION
-    aws_s3_bucket = process.env.WEBSHOT_S3_BUCKET
-    AWS.config.update({accessKeyId: aws_akid, secretAccessKey: aws_sk, region: aws_region});
-    s3bucket = new AWS.S3({params: {Bucket: "chutter-screencaps"}})
-    image_stream.on("end", function(){
-      image_data = Buffer.concat(image_buffers);
-      params = {Key: (Math.random(1000)+".jpg"), Body: image_data, ContentType: "image/jpg"}
-      s3bucket.upload(params, function(err, data){
-        if(err){
-          res.send("S3 Upload error: " + err);
-        }
-        else{
-          res.send(data["Location"]);
-        }
+
+      aws_akid = process.env.S3_AWS_ACCESS_KEY_ID
+      aws_sk = process.env.S3_AWS_SECRET_ACCESS_KEY
+      aws_region = process.env.S3_AWS_REGION
+      aws_s3_bucket = process.env.WEBSHOT_S3_BUCKET
+      AWS.config.update({accessKeyId: aws_akid, secretAccessKey: aws_sk, region: aws_region});
+      s3bucket = new AWS.S3({params: {Bucket: "chutter-screencaps"}})
+      image_stream.on("end", function(){
+        image_data = Buffer.concat(image_buffers);
+        params = {Key: (Math.random(1000)+".jpg"), Body: image_data, ContentType: "image/jpg"}
+        s3bucket.upload(params, function(err, data){
+          if(err){
+            res.send("S3 Upload error: " + err);
+          }
+          else{
+            res.send(data["Location"]);
+          }
+        })
       })
-    })
+    }
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
